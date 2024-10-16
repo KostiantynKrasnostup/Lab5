725d488247ae849d7a7bfa55e60ea93343ae5199
KostiantynKrasnostup <vladovskiy462@gmail.com>
2024-10-17 00:41:42 +0300

    Add files via upload

---
+++ Приклад 1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    interface Operations<T>
+    {
+        
+        double Add(T var1, T var2);
+        double Sub(T var1, T var2);
+    }
+
+    
+    class MyClass<T> : Operations<T>
+    {
+        public double Add(T var1, T var2)
+        {
+            double res = Convert.ToDouble(var1) + Convert.ToDouble(var2);
+            return res;
+        }
+
+        public double Sub(T var1, T var2)
+        {
+            double res = Convert.ToDouble(var1) - Convert.ToDouble(var2);
+            return res;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            MyClass<int> mc1 = new MyClass<int>();
+            double res1 = mc1.Add(23, 48);
+            Console.WriteLine("res1 = {0}", res1);
+
+            MyClass<float> mc2 = new MyClass<float>();
+            double res2 = mc2.Sub(8.804f, 1.704f);
+            Console.WriteLine("res2 = {0:f}", res2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Приклад 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Sorter
+{
+    public static void SelectionSort<T>(T[] array) where T : IComparable<T>
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            int index = i;
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j].CompareTo(array[index]) < 0)
+                {
+                    index = j;
+                }
+            }
+            T temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] a = { 2, 5, 6, 1, 3, 4 };
+        Sorter.SelectionSort(a);
+        Console.WriteLine(string.Join(", ", a));
+    }
+}
